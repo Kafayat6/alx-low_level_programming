e5a022593927a9a84e0c12d379e3e74c344e443f
Kafayat6 <101401730+Kafayat6@users.noreply.github.com>
2022-05-26 22:23:27 +0100

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,23 @@
-g
+#include "lists.h"
+
+/**
+ *free_listint2 - free a list
+ *@head: pointer points to a pointer to head
+ *
+ *Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *tmp;
+
+if (head == NULL)
+return;
+
+while (*head != NULL)
+{
+tmp = *head;
+*head = tmp->next;
+free(tmp);
+}
+}
