7f111b6d6e49f514de0b3acc6af209b8225c184b
Kafayat6 <101401730+Kafayat6@users.noreply.github.com>
2022-05-26 22:22:05 +0100

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,21 @@
-g
+#include "lists.h"
+
+/**
+ *free_listint - free list
+ *@head: pointer to head
+ *
+ *Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+listint_t *next;
+
+while (head != NULL)
+{
+next = head->next;
+free(head);
+head = next;
+
+}
+}
